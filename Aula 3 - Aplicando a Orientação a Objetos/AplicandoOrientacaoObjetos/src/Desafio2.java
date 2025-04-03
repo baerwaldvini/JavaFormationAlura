b73e44b155b9453db81e8915c834739ec493d734
Vinícius Leonardo Baerwald <vynyleo@gmail.com>
2025-04-03 07:54:41 -0300

    Desafio finalizado - Iniciando Módulo 3

--- Aula 3 - Aplicando a Orientação a Objetos/AplicandoOrientacaoObjetos/src/Desafio2.java
+++ Aula 3 - Aplicando a Orientação a Objetos/AplicandoOrientacaoObjetos/src/Desafio2.java
@@ -48,6 +48,82 @@ class IdadePessoa {
             }
         }
     }
+class Produto {
+    private String nome;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double aplicarDesconto(double desconto){
+        return preco = preco - (preco * (desconto /100));
+    }
+}
+
+class Aluno2 {
+    private String nome;
+    private double notas;
+    private int quantidadeNotas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double notas) {
+        this.notas += notas;
+        quantidadeNotas ++;
+    }
+
+    public double calcularMedia () {
+        notas = getNotas();
+        return notas / quantidadeNotas;
+    }
+}
+
+class Livro {
+    private String titulo, autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void exbirDetalhes(){
+        System.out.println("\nLivro: " + getTitulo() + " , do autor: " + getAutor());
+    }
+}
 public class Desafio2 {
     public static void main(String[] args) {
         IdadePessoa idadePessoa = new IdadePessoa();
@@ -55,5 +131,30 @@ public class Desafio2 {
         idadePessoa.setIdade(17);
         idadePessoa.setNome("Vini");
         idadePessoa.maiorIdade();
+
+        Produto produto = new Produto();
+
+        produto.setPreco(30);
+        produto.setNome("Lapis de colorir");
+
+        System.out.println("\nO produto " + produto.getNome() + " custa " + produto.getPreco() + " , com desconto fica " + produto.aplicarDesconto(25));
+
+        Aluno2 aluno = new Aluno2();
+
+        aluno.setNome("Vini");
+        aluno.setNotas(8.79);
+        aluno.setNotas(9);
+        aluno.setNotas(5.45);
+        aluno.setNotas(6);
+        aluno.setNotas(10);
+        aluno.setNotas(7.5);
+
+        System.out.println("\nO aluno " + aluno.getNome() + " ficou com a média: " + aluno.calcularMedia());
+
+        Livro livro = new Livro();
+        livro.setAutor("JK Rowling");
+        livro.setTitulo("HP e a Pedra Filosofal");
+
+        livro.exbirDetalhes();
     }
 }
